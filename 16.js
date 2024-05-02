b8dd5dccd95d39b8b54337e819f0f18d3252eb1c
hecielmedFedora <hectormedranomeza@proton.me>
2024-05-02 15:54:08 -0600

    Imprimir diez veces la serie de números del 1 al 10

---
+++ 16.js
@@ -0,0 +1,13 @@
+// Imprimir diez veces la serie de números del 1 al 10
+
+let times = 10;
+
+while (times > 0)
+{
+    times--;
+
+    for (let i = 1; i <= 10; i++)
+    {
+        console.log(i);``
+    }
+}
