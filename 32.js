c4d8aac48a3a832d032a17cf2dfc2ec8b398fa22
hecielmedFedora <hectormedranomeza@proton.me>
2024-05-02 19:16:43 -0600

    Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,10 @@
+// Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
+// debiendo ir cada uno en una fila distinta.
+
+let names = ["Infortunios", "Pesares", "Desgracias", "Desesperanzas", "Desolaciones", "Lágrimas", "Tristezas", "Amarguras", "Desamparos",
+    "Desencantos", "Desánimos", "Añoranzas", "Penurias", "Melancolías", "Desconsuelos", "Aflicciones", "Sombra", "Desalientos", "Desilusiones", "Desdichas"];
+  
+  for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+  }
+  
