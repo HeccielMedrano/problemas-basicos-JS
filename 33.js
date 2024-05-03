8379e932f8957fc22611c772989f13d0432ec534
hecielmedFedora <hectormedranomeza@proton.me>
2024-05-02 20:35:42 -0600

    Hacer un programa que lea las calificaciones de un alumno en 10 asignaturas, las almacene en un archivo  y calcule e imprima su media.

--- 33.js
+++ 33.js
@@ -1,12 +1,56 @@
 // Hacer un programa que lea las calificaciones de un alumno en 10 asignaturas, las almacene
 // en y calcule e imprima su media.
 
-let grades = [9, 4, 2, 8, 8, 7.5, 3, 2, 5, 2];
-let accumulator = 0.0;
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const fileSystem = require('fs');
 
-for (let i = 0; i < grades.length; i++)
+let grades = [];
+function getInput()
 {
-    accumulator += i;
+    readline.question('', input => { 
+        if (grades.length < 10)
+        {
+            if (/^[a-zA-Z]$/.test(input) == true)
+            {
+                console.log("No se ingreso un numero.");
+                getInput();
+            }
+            else
+            {
+                grades.push(parseFloat(input));
+                getInput();
+            }
+        }
+        else 
+        {
+            let accumulator = 0;
+            const data = grades.join('\n');
+
+            fileSystem.writeFile('grades.txt', data, (err) => {
+                if (err) throw err;
+            });
+            
+            fileSystem.readFile('grades.txt', 'utf8', (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                
+                const lines = data.split('\n');
+                
+                lines.forEach(line => {
+                    accumulator += parseFloat(lines);
+                });
+
+                console.log(accumulator/10);
+            });
+            
+            readline.close();
+        }
+    });
 }
 
-console.log(accumulator/grades.length)
+getInput();
