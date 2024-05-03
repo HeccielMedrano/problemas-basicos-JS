1c5601912a3fe183d49af39eaad774c045cef057
hecielmedFedora <hectormedranomeza@proton.me>
2024-05-02 18:37:23 -0600

    Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.

---
+++ 28.js
@@ -0,0 +1,12 @@
+// Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.
+
+let random = Math.round(Math.random());
+
+if (random == 0)
+{
+    console.log("Cara");
+}
+else
+{
+    console.log("Cruz");
+}
